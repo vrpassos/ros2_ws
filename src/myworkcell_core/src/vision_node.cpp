72f4409e8b40243f969a22c7106406cba73e0193
vrpassos <vpassos@pm.me>
2025-05-06 19:02:29 -0300

    3.3 Build a Moveit Package

--- src/myworkcell_core/src/vision_node.cpp
+++ src/myworkcell_core/src/vision_node.cpp
@@ -2,22 +2,27 @@
 **  Simple ROS Node
 **/
 #include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 #include <fake_ar_publisher/msg/ar_marker.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
 #include <myworkcell_core/srv/localize_part.hpp>
 
 class Localizer : public rclcpp::Node
 {
 public:
-  Localizer() : Node("vision_node"), last_msg_{nullptr}
+  Localizer() : Node("vision_node"), buffer_(this->get_clock()), listener_(buffer_), last_msg_{nullptr}
   {
     ar_sub_ = this->create_subscription<fake_ar_publisher::msg::ARMarker>(
-      "ar_pose_marker",
-      rclcpp::QoS(1),
-      std::bind(&Localizer::visionCallback, this, std::placeholders::_1));
+        "ar_pose_marker",
+        rclcpp::QoS(1),
+        std::bind(&Localizer::visionCallback, this, std::placeholders::_1));
 
     server_ = this->create_service<myworkcell_core::srv::LocalizePart>(
-      "localize_part",
-      std::bind(&Localizer::localizePart, this, std::placeholders::_1, std::placeholders::_2));
+        "localize_part",
+        std::bind(&Localizer::localizePart, this, std::placeholders::_1, std::placeholders::_2));
   }
 
   void visionCallback(fake_ar_publisher::msg::ARMarker::SharedPtr msg)
@@ -44,12 +49,22 @@ public:
       return;
     }
 
-    res->pose = p->pose.pose;
+    geometry_msgs::msg::PoseStamped target_pose_from_cam;
+    target_pose_from_cam.header = p->header;
+    target_pose_from_cam.pose = p->pose.pose;
+
+    geometry_msgs::msg::PoseStamped target_pose_from_req = buffer_.transform(
+        target_pose_from_cam, req->base_frame);
+
+    res->pose = target_pose_from_req.pose;
     res->success = true;
   }
 
   rclcpp::Subscription<fake_ar_publisher::msg::ARMarker>::SharedPtr ar_sub_;
   rclcpp::Service<myworkcell_core::srv::LocalizePart>::SharedPtr server_;
+  tf2_ros::Buffer buffer_;
+  tf2_ros::TransformListener listener_;
+
   fake_ar_publisher::msg::ARMarker::SharedPtr last_msg_;
 };
 
